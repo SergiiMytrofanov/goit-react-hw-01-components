9a2467c54382d6776a46c76747de2f7e71a561b6
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-07-23 17:54:58 +0300

    transaction styles

--- src/components/TransactionsHistory/Transactions.jsx
+++ src/components/TransactionsHistory/Transactions.jsx
@@ -1,48 +1,41 @@
 import PropTypes from 'prop-types';
+import styles from './Transactions.module.css';
 
-export default function TransactionHistory ({
-    title, 
-    itemsTable, 
-    }) {
-        return(
-            <section className="transaction">
-          {title && <h2 className="title">{title}</h2>}
-    
-            <table className="transaction-history">
-                <thead>
-                <tr >
-                  <th className='tableHead'>Type</th>
-                  <th className='tableHead'>Amount</th>
-                  <th className='tableHead'>Currency</th>
-                </tr>
-              </thead>
-            {itemsTable.map(({
-                id, 
-                type, 
-                amount, 
-                currency,
-            }) =>(
-    <tbody>
-    <tr key={id}>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-    </tbody>
-            ))}
-          </table>
-        </section>
-        );
-    }
-    
-    TransactionHistory.propTypes = {
-        title: PropTypes.string,
-        itemsTable: PropTypes.arrayOf(
-          PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.number.isRequired,
-            currency: PropTypes.string.isRequired,
-          })
-        ).isRequired,
-      };
+export default function TransactionHistory({ title, itemsTable }) {
+  return (
+    <section className={styles.transaction}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+
+      <table className={styles.transactionHistory}>
+        <thead>
+          <tr>
+            <th className={styles.tableHead}>Type</th>
+            <th className={styles.tableHead}>Amount</th>
+            <th className={styles.tableHead}>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itemsTable.map(({ id, type, amount, currency }) => (
+            <tr key={id} className={styles.tableRow}>
+              <td className={styles.tableCell}>{type}</td>
+              <td className={styles.tableCell}>{amount}</td>
+              <td className={styles.tableCell}>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+TransactionHistory.propTypes = {
+  title: PropTypes.string,
+  itemsTable: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
